41da60eea9776b19169e37f02068196dfee9a37a
Den <den.mitskevich@gmail.com>
2024-02-15 23:14:09 +0300

    6. Правила Checkstyle [#504981]

---
+++ src/main/java/ru/job4j/tracker/checkstyle/Broken.java
@@ -0,0 +1,25 @@
+package ru.job4j.tracker.checkstyle;
+
+public class Broken {
+    public static final String NEWVALUE = "";
+    private String surname;
+    private String name;
+    private int sizeOfempty = 10;
+
+    public Broken() {
+    }
+
+    void echo() {
+
+    }
+
+    public void media(Object obj) {
+        if (obj != null) {
+            System.out.println(obj);
+        }
+    }
+
+    public void method(int a, int b, int c, int d, int e, int f, int g) {
+
+    }
+}
