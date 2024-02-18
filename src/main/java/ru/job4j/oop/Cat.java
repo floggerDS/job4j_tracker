cf956f21f20b64d2fb0487d05a147ad3149660ba
Den <den.mitskevich@gmail.com>
2024-02-18 21:15:37 +0300

    1.5. Поля объекта [#166083 #126640]

--- src/main/java/ru/job4j/oop/Cat.java
+++ src/main/java/ru/job4j/oop/Cat.java
@@ -1,13 +1,38 @@
 package ru.job4j.oop;
 
 public class Cat {
+    private String food;
+    private String name;
+
+    public void show() {
+        System.out.println(this.name);
+        System.out.println(this.food);
+    }
+
+    public void eat(String meat) {
+        this.food = meat;
+    }
+
+    public void giveNick(String nick) {
+        this.name = nick;
+    }
+
     public String sound() {
         String voice = "may-may";
         return voice;
     }
 
     public static void main(String[] args) {
-        Cat peppy = new Cat();
-        Cat sparky = new Cat();
+        System.out.println("There are gav's food.");
+        Cat gav = new Cat();
+        gav.giveNick("Gav");
+        gav.eat("kotleta");
+        gav.show();
+
+        System.out.println("There are black's food.");
+        Cat black = new Cat();
+        black.eat("fish");
+        black.giveNick("Black");
+        black.show();
     }
 }
